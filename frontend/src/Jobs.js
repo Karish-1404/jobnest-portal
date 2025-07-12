5ff11951d2731e6e3003f5534e888c3c10ffafbf
karish-1404 <lakhskarish@gmail.com>
2025-07-12 11:32:40 +0530

    Fix Netlify build: removed unused FaBuilding

--- frontend/src/Jobs.js
+++ frontend/src/Jobs.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { FaBuilding, FaMapMarkerAlt, FaEye } from 'react-icons/fa';
 import './Jobs.css';
 
 function Jobs() {

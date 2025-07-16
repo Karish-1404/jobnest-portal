85304c8a8ff503c6666b22e8c9c8f00e4120c905
karish-1404 <lakhskarish@gmail.com>
2025-07-16 22:45:24 +0530

    Added Cloudinary upload for job logos

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: ' djkrv5zee',
+  api_key: '178532493722967',
+  api_secret: 'owjybmIki9OxtYqCiOgsYk5jOaM'
+});
+
+module.exports = cloudinary;
